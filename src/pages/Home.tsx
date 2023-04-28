cadb3e881aaf505dd3b98cc12fcfccd18aa13b9e
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-04-28 16:45:52 +0700

    Changed home button colors

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,13 +25,13 @@ export default function Home() {
                     className="flex flex-col sm:flex-row items-center justify-center space-y-4 sm:space-y-0 sm:space-x-4 mt-8">
                     <Link to={'/all'}>
                         <button
-                            className="w-full sm:w-auto px-8 py-3 text-base sm:text-xl font-medium bg-purple-600 hover:bg-purple-500 rounded-full transition-colors duration-300">
+                            className="w-full sm:w-auto px-8 py-3 text-base sm:text-xl font-medium bg-gray-800 hover:bg-purple-700 rounded-full transition-colors duration-300">
                             View All Stats
                         </button>
                     </Link>
                     <Link to={'/player'}>
                         <button
-                            className="w-full sm:w-auto px-8 py-3 text-base sm:text-xl font-medium bg-purple-600 hover:bg-purple-500 rounded-full transition-colors duration-300">
+                            className="w-full sm:w-auto px-8 py-3 text-base sm:text-xl font-medium bg-gray-800 hover:bg-purple-700 rounded-full transition-colors duration-300">
                             Check Player Stats
                         </button>
                     </Link>
